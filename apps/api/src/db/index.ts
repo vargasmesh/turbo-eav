96169b4fbbd081d932d9be6e824bd6156545453d
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2023-04-11 18:30:41 -0300

    Feat: Add drizzle connection

---
+++ apps/api/src/db/index.ts
@@ -0,0 +1,10 @@
+import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const db = drizzle(pool);
+
+export { db };
