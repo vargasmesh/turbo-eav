983afc44700565bea8ca357482a19bd88b5cf713
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2023-04-11 20:15:45 -0300

    Feat: Add tailwindcss

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,7 @@ import { render } from "preact";
 import { App } from "./app";
 import { queryClient, trpc, trpcClient } from "./trpc";
 import { QueryClientProvider } from "@tanstack/react-query";
+import "./index.css";
 
 render(
   <trpc.Provider client={trpcClient} queryClient={queryClient}>
