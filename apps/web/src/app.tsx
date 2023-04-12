82b756a45d3f430df7596ae0abca14f43f3f2b01
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2023-04-11 21:49:56 -0300

    Feat: Style entities list

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,13 +1,40 @@
+import { FunctionalComponent } from "preact";
 import { trpc } from "./trpc";
+
+type EntityProps = {
+  entity: {
+    id: number;
+    name: string;
+  };
+};
+
+const Entity: FunctionalComponent<EntityProps> = ({ entity }) => {
+  return (
+    <div className="bg-white md:max-w-sm rounded-xl p-6 shadow-2xl font-mono">
+      <div>
+        <span className="text-gray-500">ID: </span>
+        {entity.id}
+      </div>
+      <div>
+        <span className="text-gray-500">Entity: </span>
+        <span className="font-semibold">{entity.name}</span>
+      </div>
+    </div>
+  );
+};
 
 export function App() {
   const entitiesQuery = trpc.listEntities.useQuery();
 
   return (
-    <>
-      {entitiesQuery.data?.map((e) => {
-        return <div key={e.id}>{e.name}</div>;
-      })}
-    </>
+    <div className="h-screen bg-gradient-to-tr from-[#F3E9EF] to-[#1B6678]">
+      <div className="container mx-auto pt-20 px-6">
+        <div className="flex flex-col gap-10">
+          {entitiesQuery.data?.map((e) => {
+            return <Entity entity={e} />;
+          })}
+        </div>
+      </div>
+    </div>
   );
 }
